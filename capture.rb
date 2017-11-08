a9ae76bb535e9317b4464fb0de857bf6d83e6b30
Payscout Inc <victor@payscout.com>
2017-11-08 16:40:38 +0200

    Update capture.rb

--- capture.rb
+++ capture.rb
@@ -10,7 +10,7 @@ http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 request = Net::HTTP::Post.new(url)
 request["content-type"] = 'application/x-www-form-urlencoded'
 request["cache-control"] = 'no-cache'
-request.body = "client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=CAPTURE¤cy=USD&initial_amount=99.99&original_transaction_id=A000133RY3R&billing_invoice_number=1999"
+request.body = "client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=CAPTURE&currency=USD&initial_amount=99.99&original_transaction_id=A000133RY3R&billing_invoice_number=1999"
 
 response = http.request(request)
 puts response.read_body 
